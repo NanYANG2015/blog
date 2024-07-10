c269c7d060eb34d3f0599d91729d75989c9c10a8
nanyang <yangnanangle@gmail.com>
2024-07-10 23:17:23 +0800

    Site updated: 2024-07-10 23:17:13

--- js/matery.js
+++ js/matery.js
@@ -75,30 +75,30 @@ $(function () {
         $('#articleContent img').each(function () {
             let imgPath = $(this).attr('src');
             $(this).wrap('<div class="img-item" data-src="' + imgPath + '" data-sub-html=".caption"></div>');
-        //     // 图片添加阴影
-        //     $(this).addClass("img-shadow img-margin");
-        //     // 图片添加字幕
-        //     let alt = $(this).attr('alt');
-        //     let title = $(this).attr('title');
-        //     let captionText = "";
-        //     // 如果alt为空，title来替
-        //     if (alt === undefined || alt === "") {
-        //         if (title !== undefined && title !== "") {
-        //             captionText = title;
-        //         }
-        //     } else {
-        //         captionText = alt;
-        //     }
-        //     // 字幕不空，添加之
-        //     if (captionText !== "") {
-        //         let captionDiv = document.createElement('div');
-        //         captionDiv.className = 'caption';
-        //         let captionEle = document.createElement('b');
-        //         captionEle.className = 'center-caption';
-        //         captionEle.innerText = captionText;
-        //         captionDiv.appendChild(captionEle);
-        //         this.insertAdjacentElement('afterend', captionDiv)
-        //     }
+            // 图片添加阴影
+            $(this).addClass("img-shadow img-margin");
+            // 图片添加字幕
+            let alt = $(this).attr('alt');
+            let title = $(this).attr('title');
+            let captionText = "";
+            // 如果alt为空，title来替
+            if (alt === undefined || alt === "") {
+                if (title !== undefined && title !== "") {
+                    captionText = title;
+                }
+            } else {
+                captionText = alt;
+            }
+            // 字幕不空，添加之
+            if (captionText !== "") {
+                let captionDiv = document.createElement('div');
+                captionDiv.className = 'caption';
+                let captionEle = document.createElement('b');
+                captionEle.className = 'center-caption';
+                captionEle.innerText = captionText;
+                captionDiv.appendChild(captionEle);
+                this.insertAdjacentElement('afterend', captionDiv)
+            }
         });
         $('#articleContent, #myGallery').lightGallery({
             selector: '.img-item',
@@ -170,3 +170,20 @@ $(function () {
     // 初始化加载 tooltipped.
     $('.tooltipped').tooltip();
 });
+
+//黑夜模式提醒开启功能
+setTimeout(function () {
+    if ((new Date().getHours() >= 19 || new Date().getHours() < 7) && !$('body').hasClass('DarkMode')) {
+        let toastHTML = '<span style="color:#97b8b2;border-radius: 10px;>' + '<i class="fa fa-bellaria-hidden="true"></i>晚上使用深色模式阅读更好哦。(ﾟ▽ﾟ)</span>'
+        M.toast({ html: toastHTML })
+    }
+}, 2200);
+
+//黑夜模式判断
+if (localStorage.getItem('isDark') === '1') {
+    document.body.classList.add('DarkMode');
+    $('#sum-moon-icon').addClass("fa-sun").removeClass('fa-moon')
+} else {
+    document.body.classList.remove('DarkMode');
+    $('#sum-moon-icon').removeClass("fa-sun").addClass('fa-moon')
+}
